ec85f77090953f66a292543193285ae908365cf0
DrChenXX <36144128+DrChenXX@users.noreply.github.com>
2022-11-13 19:20:18 +0800

    db

--- test/src/main/java/database/ConnectDB.java
+++ test/src/main/java/database/ConnectDB.java
@@ -3,6 +3,10 @@ package database;
 import interfaces.Callable;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ConnectDB implements Callable {
     private static ConnectDB instance;
@@ -27,42 +31,65 @@ public class ConnectDB implements Callable {
     public void call(){
 
     }
-
-    public static ResultSet getResultSet(String sql) {
-        Connection connection = null;
-        try {
-            //加载mysql的驱动类
+    public static List<Map<String, Object>> getList(String sql) {
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+        Connection conn = null;
+        Statement sta = null;
+        ResultSet rs = null;
+        try{
             Class.forName(JDBC_DRIVER);
-            //获取数据库连接
-            connection = DriverManager.getConnection(DB_URL, USER, PASS);
-            PreparedStatement prst = connection.prepareStatement(sql);
-            //结果集
-            ResultSet rs = prst.executeQuery();
-            prst.close();
-            return rs;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            sta = conn.createStatement();
+            rs = sta.executeQuery(sql);
+            ResultSetMetaData md = rs.getMetaData();
+            int columnCount = md.getColumnCount();
+            while(rs.next()) {
+                Map<String, Object> rowData = new HashMap<String, Object>();
+                for (int i = 1; i <= columnCount; i++) {
+                    rowData.put(md.getColumnName(i), rs.getObject(i));
                 }
+                list.add(rowData);
             }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        return null;
+        return list;
     }
+//    public static ResultSet getResultSet(String sql) {
+//        Connection connection = null;
+//        try {
+//            //加载mysql的驱动类
+//            Class.forName(JDBC_DRIVER);
+//            //获取数据库连接
+//            connection = DriverManager.getConnection(DB_URL, USER, PASS);
+//            PreparedStatement prst = connection.prepareStatement(sql);
+//            //结果集
+//            ResultSet rs = prst.executeQuery();
+//            prst.close();
+//            return rs;
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }finally {
+//            if (connection != null) {
+//                try {
+//                    connection.close();
+//                } catch (SQLException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//        return null;
+//    }
 
-    public static void main(String[] args) {
-        ResultSet rs = getResultSet("SELECT ID FROM T_ERROR");
-        try {
-            while (rs.next()){
-                System.out.println("ID:"+rs.getString("ID"));
-            }
-        } catch (Exception a ) {
-
-        }
-
-    }
+//    public static void main(String[] args) {
+//        List<Map<String, Object>> list = queryAll("SELECT * FROM T_ERROR");
+//        for (String s : list.get(0).keySet()) {
+//            System.out.println(s);
+//        }
+//        System.out.println(list.get(0).get("ID"));
+//        System.out.println(list.get(0).get("SHIJIAN"));
+//        System.out.println(list.get(0).get("CONTENT"));
+//    }
 }
