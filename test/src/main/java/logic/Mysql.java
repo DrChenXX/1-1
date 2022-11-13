e85f52ec553136fd8858c5ee290b9297f23f95e0
DrChenXX <36144128+DrChenXX@users.noreply.github.com>
2022-11-13 18:08:56 +0800

    1

--- test/src/main/java/logic/Mysql.java
+++ test/src/main/java/logic/Mysql.java
@@ -10,59 +10,32 @@ public class Mysql {
     static final String PASS = "Cxy_20020208";
 
     public static void main(String[] args) {
-        Connection conn = null;
-        Statement stmt = null;
-        try{
-            System.out.println("1");
-            // 注册 JDBC 驱动
+        Connection connection = null;
+        try {
+            //加载mysql的驱动类
             Class.forName(JDBC_DRIVER);
-
-            // 打开连接
-            System.out.println("连接数据库...");
-            conn = DriverManager.getConnection(DB_URL,USER,PASS);
-
-            // 执行查询
-            System.out.println("实例化Statement对象...");
-            stmt = conn.createStatement();
-            String sql;
-            sql = "select * from T_BIYEYAOQIU";
-            ResultSet rs = stmt.executeQuery(sql);
-
-            // 展开结果集数据库
-            while(rs.next()){
-                // 通过字段检索
-                String id = rs.getNString("ID");
-                String peiyangfanganID = rs.getNString("PEIYANGFANGAN_ID");
-                String content = rs.getNString("CONTENT");
-
-                // 输出数据
-                System.out.println("ID:" + id);
-                System.out.println("PEIYANGFANGAN_ID:" + peiyangfanganID);
-                System.out.println("CONTENT:" + content);
+            //获取数据库连接
+            connection = DriverManager.getConnection(DB_URL, USER, PASS);
+            //mysql查询语句
+            String sql = "SELECT ID FROM T_ERROR";
+            PreparedStatement prst = connection.prepareStatement(sql);
+            //结果集
+            ResultSet rs = prst.executeQuery();
+            while (rs.next()) {
+                System.out.println("ID:" + rs.getString("ID"));
             }
-
-            // 完成后关闭连接
             rs.close();
-            stmt.close();
-            conn.close();
-        }catch (SQLException se){
-            // 处理 JDBC 错误
-            se.printStackTrace();
-        }catch (Exception e){
-            // 处理 Class.forName 错误
+            prst.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }finally {
-            // 关闭资源
-            try{
-                if (stmt != null) stmt.close();
-            }catch (SQLException se2){
-            }//什么都不做
-            try{
-                if(conn != null) conn.close();
-            }catch (SQLException se){
-                se.printStackTrace();
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
             }
         }
-        System.out.println("连接数据库结束！");
     }
 }
