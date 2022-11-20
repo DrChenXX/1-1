63b3bd9df4146dc5deb7830322a183473ebaed6f
iLoner121 <edguodes@outlook.com>
2022-11-21 00:44:57 +0800

    简易登录功能以及程序入口

--- test/src/main/java/com/example/test/model/service/HomeService.java
+++ test/src/main/java/com/example/test/model/service/HomeService.java
@@ -2,11 +2,16 @@ package com.example.test.model.service;
 
 import com.example.test.datatype.LoginRequest;
 import com.example.test.interfaces.UserService;
+import com.example.test.model.dao.logic.YonghuMgr;
+import com.example.test.model.entity.Yonghu;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 @Service("homeService")
 public class HomeService implements UserService {
+    @Autowired
+    private YonghuMgr yonghuMgr;
 
 
     @Override
@@ -16,8 +21,16 @@ public class HomeService implements UserService {
     }
 
     public boolean login(LoginRequest data) {
-
-        return true;
+        Yonghu yonghu = yonghuMgr.getByID(data.getUsername());
+        if(yonghu==null) {
+            return false;
+        }
+        String password = yonghu.getPassword();
+        if (data.getPassword().equals(password)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
