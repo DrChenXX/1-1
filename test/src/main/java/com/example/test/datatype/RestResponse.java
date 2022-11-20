dfcc595d302deabeacf0a93c513e273a0a112355
iLoner121 <edguodes@outlook.com>
2022-11-20 23:09:11 +0800

    统一json格式

---
+++ test/src/main/java/com/example/test/datatype/RestResponse.java
@@ -0,0 +1,72 @@
+package com.example.test.datatype;
+
+import javax.lang.model.type.NullType;
+import java.io.Serializable;
+
+// 统一json返回接口
+public class RestResponse<T> implements Serializable {
+
+    private Integer status;
+    private String msg;
+    private T data;
+
+    public RestResponse(){
+
+    }
+
+    public RestResponse(int status, String message) {
+        this.setStatus(status);
+        this.setMsg(message);
+    }
+
+    public RestResponse(int status, String message, T data) {
+        this.setStatus(status);
+        this.setMsg(message);
+        this.setData(data);
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "RestResponse{" + "code=" + status + ", msg='" + msg + '\'' +", data=" + data +'}';
+    }
+
+    public static <T> RestResponse<T> success(String msg) {
+        return new RestResponse<T>(200,msg,null);
+    }
+
+    public static <T> RestResponse<T> success(String msg, T data) {
+        return new RestResponse<T>(200,msg,data);
+    }
+
+    public static <T> RestResponse<T> fail(String msg) {
+        return new RestResponse<T>(500,msg,null);
+    }
+
+    public static <T> RestResponse<T> fail(String msg, T data) {
+        return new RestResponse<T>(500,msg,data);
+    }
+}
--- test/src/main/java/com/example/test/model/service/HomeService.java
+++ test/src/main/java/com/example/test/model/service/HomeService.java
@@ -13,4 +13,6 @@ public class HomeService implements UserService {
         System.out.println("homeService");
         return  "homeService";
     }
+
+
 }
