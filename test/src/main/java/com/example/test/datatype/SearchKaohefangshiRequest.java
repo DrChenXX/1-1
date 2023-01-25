43452ec89f7b6577e6167baed1f4296d9f35f882
Libeny <87231808+Libeny@users.noreply.github.com>
2023-01-25 23:33:53 +0800

    Add files via upload

---
+++ test/src/main/java/com/example/test/datatype/SearchKaohefangshiRequest.java
@@ -0,0 +1,24 @@
+package com.example.test.datatype;
+
+public class FeedbackMatrixOfBiyeyaoqiuRequest {
+    private String kechengid;
+
+    public FeedbackMatrixOfBiyeyaoqiuRequest() {
+    }
+
+    public FeedbackMatrixOfBiyeyaoqiuRequest(String kechengid) {
+        this.kechengid = kechengid;
+    }
+
+    public String getMatrixOfBiyeyaoqiu() {
+        return kechengid;
+    }
+
+    @Override
+    public String toString() {
+        return "FeedbackMatrixOfBiyeyaoqiuRequest{" +
+                "feedbackMatrixOfBiyeyaoqiuRequest='" + kechengid + '\'' +
+                '}';
+    }
+}
+
