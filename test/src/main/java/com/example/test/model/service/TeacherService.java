89c558a979e5ae85d569c8c25f785b6a2c8a1d68
Libeny <87231808+Libeny@users.noreply.github.com>
2023-02-03 12:17:05 +0800

    Add files via upload

--- test/src/main/java/com/example/test/model/service/TeacherService.java
+++ test/src/main/java/com/example/test/model/service/TeacherService.java
@@ -1,12 +1,6 @@
 package com.example.test.model.service;
 
-import com.example.test.datatype.RestResponse;
-import com.example.test.datatype.SearchPeiyangmubiaoRequest;
 import com.example.test.interfaces.UserService;
-import com.example.test.model.dao.logic.BiyeyaoqiuMgr;
-import com.example.test.model.dao.logic.DangqiankechengMgr;
-import com.example.test.model.dao.logic.PeiyangmubiaoMgr;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
@@ -16,7 +10,13 @@ public class TeacherService implements UserService {
     @Autowired
     private PeiyangmubiaoMgr peiyangmubiaoMgr;
     @Autowired
+    private DangqiankechengMgr dangqiankechengMgr;
+    @Autowired
+    private PeiyangmubiaoMgr peiyangmubiaoMgr;
+    @Autowired
     private BiyeyaoqiuMgr biyeyaoqiuMgr;
+    @Autowired
+    private KechengduizhibiaodianMgr kechengduizhibiaodianMgr;
     @Override
     public String name() {
         System.out.println("teacherService");
@@ -27,6 +27,135 @@ public class TeacherService implements UserService {
         return "";
     }
 
+    public RestResponse searchPeiyangmubiao(SearchPeiyangmubiaoRequest request) {
+        List<Peiyangmubiao> peiyangmubiaos = peiyangmubiaoMgr.getByPEIYANGFANGANID(request.getPeiyangfangan());
+        if (peiyangmubiaos.isEmpty()) {
+            return new RestResponse().fail("没有找到对应培养目标");
+        }
+        List<SearchPeiyangmubiaoResponse> responses = new ArrayList<SearchPeiyangmubiaoResponse>();
+        int n = 1;
+        for (Peiyangmubiao peiyangmubiao : peiyangmubiaos) {
+            responses.add(new SearchPeiyangmubiaoResponse(n,peiyangmubiao.getContent()));
+            n++;
+        }
+        return new RestResponse().success("已找到对应的培养目标",responses);
+    }
 
+    public RestResponse searchDangqiankecheng (SearchDangqiankechengRequest request) {
+        List<Dangqiankecheng> dangqiankechengs = DangqiankechengMgr.get(request.getDangqiankecheng());
+        if (dangqiankechengs.isEmpty()) {
+            return new RestResponse().fail("没有找到课程");
+        }
+        List<SearchDangqiankechengResponse> responses = new ArrayList<SearchDangqiankechengResponse>();
+        int n = 1;
+        for (Dangqiankecheng dangqiankecheng : dangqiankechengs) {
+            responses.add(new SearchDangqiankechengResponse(n,dangqiankecheng.getContent()));
+            n++;
+        }
+        return new RestResponse().success("已找到对应的课程",responses);
+    }
+
+    public RestResponse confirmMatrixOfBiyeyaoqiu (ConfirmMatrixOfBiyeyaoqiuRequest request) {
+        List<Kechengduizhibiaodian> kechengduizhibiaodians = KechengduizhibiaodianMgr.getByKechengID(request.getKechengid());
+        if (kechengduizhibiaodians.isEmpty()) {
+            return new RestResponse().fail("当前课程无需支撑指标点");
+        }
+        List<ConfirmMatrixOfBiyeyaoqiuResponse> responses = new ArrayList<ConfirmMatrixOfBiyeyaoqiuResponse>();
+        int n = 1;
+        for (Kechengduizhibiaodian kechengduizhibiaodian : kechengduizhibiaodians) {
+            responses.add(new ConfirmMatrixOfBiyeyaoqiuResponse(n,kechengduizhibiaodian.getContent()));
+            n++;
+        }
+        return new RestResponse().success("确定当前课程所有支撑指标点",responses);
+    }
+
+    public RestResponse searchKechengmubiao (SearchKechengmubiaoRequest request) {
+        List<Peiyangmubiao> peiyangmubiaos = peiyangmubiaoMgr.get(request.getDangqiankecheng());
+        if (peiyangmubiaos.isEmpty()) {
+            return new RestResponse().fail("没有找到课程目标");
+        }
+        List<SearchPeiyangmubiaosResponse> responses = new ArrayList<SearchDangqiankechengResponse>();
+        int n = 1;
+        for (Peiyangmubiaos peiyangmubiao : peiyangmubiaos) {
+            responses.add(new SearchPeiyangmubiaosResponse(n,dangqiankecheng.getContent()));
+            n++;
+        }
+        return new RestResponse().success("已找到对应课程目标",responses);
+    }
+
+    //task1-1:确认毕业要求矩阵
+    public RestResponse confirmMatrixOfBiyeyaoqiu (ConfirmMatrixOfBiyeyaoqiuRequest request) {
+        List<> peiyangmubiaos = peiyangmubiaoMgr.get(request.getDangqiankecheng());
+        if (peiyangmubiaos.isEmpty()) {
+            return new RestResponse().fail("");
+        }
+        List<SearchPeiyangmubiaosResponse> responses = new ArrayList<SearchDangqiankechengResponse>();
+        int n = 1;
+        for (Peiyangmubiaos peiyangmubiao : peiyangmubiaos) {
+            responses.add(new SearchPeiyangmubiaosResponse(n,dangqiankecheng.getContent()));
+            n++;
+        }
+        return new RestResponse().success("",responses);
+    }
+
+    //feedback
+    public RestResponse confirmMatrixOfBiyeyaoqiu (ConfirmMatrixOfBiyeyaoqiuRequest request) {
+        List<> peiyangmubiaos = peiyangmubiaoMgr.get(request.getDangqiankecheng());
+        if (peiyangmubiaos.isEmpty()) {
+            return new RestResponse().fail("");
+        }
+        List<SearchPeiyangmubiaosResponse> responses = new ArrayList<SearchDangqiankechengResponse>();
+        int n = 1;
+        for (Peiyangmubiaos peiyangmubiao : peiyangmubiaos) {
+            responses.add(new SearchPeiyangmubiaosResponse(n,dangqiankecheng.getContent()));
+            n++;
+        }
+        return new RestResponse().success("",responses);
+    }
+
+    //save
+
+
+    public RestResponse confirmMatrixOfBiyeyaoqiu (ConfirmMatrixOfBiyeyaoqiuRequest request) {
+        List<> peiyangmubiaos = peiyangmubiaoMgr.get(request.getDangqiankecheng());
+        if (peiyangmubiaos.isEmpty()) {
+            return new RestResponse().fail("");
+        }
+        List<SearchPeiyangmubiaosResponse> responses = new ArrayList<SearchDangqiankechengResponse>();
+        int n = 1;
+        for (Peiyangmubiaos peiyangmubiao : peiyangmubiaos) {
+            responses.add(new SearchPeiyangmubiaosResponse(n,dangqiankecheng.getContent()));
+            n++;
+        }
+        return new RestResponse().success("",responses);
+    }
+
+    public RestResponse confirmMatrixOfBiyeyaoqiu (ConfirmMatrixOfBiyeyaoqiuRequest request) {
+        List<> peiyangmubiaos = peiyangmubiaoMgr.get(request.getDangqiankecheng());
+        if (peiyangmubiaos.isEmpty()) {
+            return new RestResponse().failM("");
+    }
+    List<SearchPeiyangmubiaosResponse> responses = new ArrayList<SearchDangqiankechengResponse>();
+    int n = 1;
+        for (Peiyangmubiaos peiyangmubiao : peiyangmubiaos) {
+        responses.add(new SearchPeiyangmubiaosResponse(n,dangqiankecheng.getContent()));
+        n++;
+    }
+        return new RestResponse().success("",responses);
+}
+
+    public RestResponse searchKechengmubiao (SearchKechengmubiaoRequest request) {
+        List<Peiyangmubiao> peiyangmubiaos = peiyangmubiaogr.get(request.getDangqiankecheng());
+        if (peiyangmubiaos.isEmpty()) {
+            return new RestResponse().fail("没有找到课程目标");
+        }
+        List<SearchPeiyangmubiaosResponse> responses = new ArrayList<SearchDangqiankechengResponse>();
+        int n = 1;
+        for (Peiyangmubiaos peiyangmubiao : peiyangmubiaos) {
+            responses.add(new SearchPeiyangmubiaosResponse(n, dangqiankecheng.getContent()));
+            n++;
+        }
+        return new RestResponse().success("已找到对应课程目标", responses);
+    }
 
 }
