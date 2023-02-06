ff2e951d13dd945b12017bd0092f2ea6e2ac2698
Libeny <87231808+Libeny@users.noreply.github.com>
2023-02-06 21:17:06 +0800

    Add files via upload

--- test/src/main/java/com/example/test/controller/TeacherController.java
+++ test/src/main/java/com/example/test/controller/TeacherController.java
@@ -1,10 +1,12 @@
 package com.example.test.controller;
 
+import com.example.test.datatype.ConfirmMatrixOfBiyeyaoqiuRequest;
+import com.example.test.datatype.RestResponse;
+import com.example.test.datatype.SearchPeiyangmubiaoRequest;
 import com.example.test.interfaces.UserController;
 import com.example.test.model.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/home/teacher")
@@ -25,6 +27,12 @@ public class TeacherController implements UserController {
         return teacherService.getRequestList(kechengmingcheng,kechengbianhao,kaikexueqi,peiyangfangan,dangqianrenwu,renwuzhuangtai);
     }
 
+    @ResponseBody
+    @RequestMapping(path = "/Kechengduizhibiaodian/ConfirmMatrixOfBiyeyaoqiu", method = RequestMethod.POST)
+    public RestResponse searchPeiyangfangan(@RequestBody ConfirmMatrixOfBiyeyaoqiuRequest request) {
+        System.out.println(request);
+        return teacherService.confirmMatrixOfBiyeyaoqiu(request);
+    }
 
 
 }
