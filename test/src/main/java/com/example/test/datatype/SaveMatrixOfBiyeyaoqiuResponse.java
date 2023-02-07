d93865befcaf6e6d15e17dfdecc852f3e84527ac
Libeny <87231808+Libeny@users.noreply.github.com>
2023-02-07 19:49:42 +0800

    Add files via upload

--- test/src/main/java/com/example/test/datatype/SaveMatrixOfBiyeyaoqiuResponse.java
+++ test/src/main/java/com/example/test/datatype/SaveMatrixOfBiyeyaoqiuResponse.java
@@ -4,61 +4,39 @@ import java.util.List;
 
 //重写中 未完成
 public class SaveMatrixOfBiyeyaoqiuResponse {
-    private String zhichengdian;
-    private boolean is_zhicheng;
-    private String xiugai;
-    private String yijian;
+    private String kechengid;
+    private List<String> mubiao;
 
     public SaveMatrixOfBiyeyaoqiuResponse() {
     }
 
-    public SaveMatrixOfBiyeyaoqiuResponse(String zhichengdian, boolean is_zhicheng, String xiugai, String yijian) {
-        this.zhichengdian = zhichengdian;
-        this.is_zhicheng = is_zhicheng;
-        this.xiugai = xiugai;
-        this.yijian = yijian;
+    public SaveMatrixOfBiyeyaoqiuResponse(String kechengid,List<String> mubiao) {
+        this.kechengid = kechengid;
+        this.mubiao = mubiao;
     }
 
-    public String getZhichengdian() {
-        return zhichengdian;
+    public String getKechengid() {
+        return kechengid;
     }
 
-    public void setZhichengdian(String zhichengdian) {
-        this.zhichengdian = zhichengdian;
+    public void setKechengid(String kechengid) {
+        this.kechengid = kechengid;
     }
-
-    public boolean getIs_zhicheng() {
-        return is_zhicheng;
+    public List<String> getMubiao() {
+        return mubiao;
     }
-
-    public void setIs_zhicheng(boolean is_zhicheng) {
-        this.is_zhicheng = is_zhicheng;
+    public void setMubiao(List<String> mubiao){
+        this.mubiao = mubiao;
     }
-
-    public String getXiugai() {
-        return xiugai;
-    }
-
-    public void setXiugai(String xiugai) {
-        this.xiugai = xiugai;
-    }
-
-    public String getYijian() {
-        return yijian;
-    }
-
-    public void setYijian(String yijian) {
-        this.yijian = yijian;
+    public void addMubiao(String mubiao) {
+        this.mubiao.add(mubiao);
     }
 
     @Override
     public String toString() {
         return "SaveMatrixOfBiyeyaoqiuResponse{" +
-                "zhichengdian=" + zhichengdian +
-                ", is_zhicheng='" + is_zhicheng +
-                ", xiugai=" + xiugai +
-                ", yijian=" + yijian + '\'' +
+                "kechengid=" + kechengid +
+                "mubiao='" + mubiao + '\'' +
                 '}';
-
     }
 }
