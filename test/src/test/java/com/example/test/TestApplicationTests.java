70f2cd8196df3d6e020993f2addcbc98d61549c2
DrChenXX <36144128+DrChenXX@users.noreply.github.com>
2023-02-12 03:17:26 +0800

    任务全部发布的bug

--- test/src/test/java/com/example/test/TestApplicationTests.java
+++ test/src/test/java/com/example/test/TestApplicationTests.java
@@ -4,6 +4,7 @@ import com.example.test.datatype.RestResponse;
 import com.example.test.datatype.SearchTeacherRequest;
 import com.example.test.datatype.SearchTeacherResponse;
 import com.example.test.datatype.SearchXueshengRequest;
+import com.example.test.model.dao.logic.RenwuMgr;
 import com.example.test.model.dao.logic.XiaoxiMgr;
 import com.example.test.model.dao.logic.YonghuMgr;
 import com.example.test.model.entity.Xiaoxi;
@@ -21,24 +22,7 @@ class TestApplicationTests {
     void contextLoads() {
     }
     public static void main(String[] args) {
-        YonghuMgr yonghuMgr = new YonghuMgr();
-        List<Yonghu> teachers = yonghuMgr.getAll();
-        System.out.println(teachers);
-        List<SearchTeacherResponse> responses = new ArrayList<SearchTeacherResponse>();
-
-        for (Yonghu teacher : teachers) {
-            responses.add(new SearchTeacherResponse(teacher.getId(),teacher.getName(),"教授","信息科学与工程学院"));
-        }
-
-        SearchXueshengRequest request = new SearchXueshengRequest("1","1","1","1","1");
-        //页码处理
-        int yeshu;
-        if(request.getYeshu() != "") { yeshu = Integer.valueOf(request.getYeshu()) - 1; }
-        else { yeshu = 0; }
-        List<SearchTeacherResponse> responses1 =
-                responses.subList(yeshu * 5, (responses.size() - (yeshu + 1) * 5) < 0 ? responses.size() : (yeshu + 1) * 5);
-        for (SearchTeacherResponse searchTeacherResponse : responses1) {
-            System.out.println(searchTeacherResponse.getId() + "  " + searchTeacherResponse.getName() + "   " + searchTeacherResponse.getZhicheng() + "  " + searchTeacherResponse.getYuanxi());
-        }
+        RenwuMgr renwuMgr = new RenwuMgr();
+        renwuMgr.updateByID("11");
     }
 }
